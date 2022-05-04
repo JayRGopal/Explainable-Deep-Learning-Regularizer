638132c3322f54a57981c2037321a51f1d9efdb2
Yousef Elgodamy <yoyoel789@gmail.com>
2022-05-04 12:59:22 -0400

    NEW SCNN results - all pth files uploaded

---
+++ OLD_Simple_CNN_Results/Control-10e/cnn.py
@@ -0,0 +1,78 @@
+from torch import nn
+import torch.nn.functional as F
+
+class SimpleCNN(nn.Module):
+    """
+    Class representing convolutional neural network through the PyTorch DL framework
+    """
+
+    def __init__(self, input_channels = 3, class_num = 10) -> None:
+        """
+        Initializes convolutional layers, pooling layers, and linear layers for
+        the SimpleCNN class. Inherits from nn.Module initializer function.
+        :param input_channels: number of color channels. Default 3 for RGB
+        :param class_num: number of classes that will be predicted by the model.
+            Default 10 for the 10 classes represented by CIFAR10
+        """
+
+        super().__init__()
+
+        # self.conv_layer1 = nn.Conv2d(input_channels, 16, 4, 1)
+        # self.pool1 = nn.MaxPool2d(3, 2)
+        # self.conv_layer2 = nn.Conv2d(16, 16, 4, 1)
+        # self.pool2 = nn.MaxPool2d(3, 1)
+        # self.conv_layer3 = nn.Conv2d(16, 32, 3, 1)
+        # self.conv_layer4 = nn.Conv2d(32,64, 3, 1)
+
+        # self.linear1 = nn.Linear(64*5*5, 32*5*5)
+        # self.linear2 = nn.Linear(32*5*5, 16*6*6)
+        # self.linear3 = nn.Linear(16*6*6, 8*6*6)
+        # self.linear4 = nn.Linear(8*6*6, 144)
+        # self.linear5 = nn.Linear(144, 50)
+        # self.linear6 = nn.Linear(50, class_num)
+        # self.linear7 = nn.Linear(class_num, class_num)
+
+        self.conv_layer1 = nn.Conv2d(input_channels, 16, 8, 1)
+        self.pool1 = nn.MaxPool2d(3, 2)
+        self.conv_layer2 = nn.Conv2d(16, 16, 5, 1)
+        self.pool2 = nn.MaxPool2d(2, 1)
+        self.conv_layer3 = nn.Conv2d(16, 32, 3, 1)
+        # self.conv_layer4 = nn.Conv2d(32,64, 3, 1)
+
+        self.linear1 = nn.Linear(800, 128)
+        self.linear2 = nn.Linear(128, 90)
+        self.linear3 = nn.Linear(90, 40)
+        self.linear4 = nn.Linear(40, 40)
+        self.linear5 = nn.Linear(40, 20)
+        self.linear6 = nn.Linear(20, class_num)
+        self.linear7 = nn.Linear(class_num, class_num)
+        
+    
+    def forward(self, X):
+        """
+        Performs forward propagation through the defined CNN layers for given input
+        :param X: Input to the CNN network. Dimension (batch size, 3 , 32, 32)
+        :return: (batch size, 10) torch tensor output of convolution and 
+        subsequent linear layers. Size 10 given the 10 predicted classes.
+        """
+        
+        # Pass through convolutional layers, with relu activation
+        conv_output = self.pool1(F.gelu(self.conv_layer1(X)))
+        conv_output = self.pool2(F.gelu(self.conv_layer2(conv_output)))
+        conv_output = F.gelu(self.conv_layer3(conv_output))
+        # conv_output = F.gelu(self.conv_layer4(conv_output))
+        
+        
+        # Pass through linear layers.
+        # RELU for every layer except last. Softmax for the last layer.
+        vec_output = nn.Flatten()(conv_output)
+        vec_output = F.gelu(self.linear1(vec_output))
+        vec_output = F.gelu(self.linear2(vec_output))
+        vec_output = F.gelu(self.linear3(vec_output))
+        vec_output = F.gelu(self.linear4(vec_output))
+        vec_output = F.gelu(self.linear5(vec_output))
+        vec_output = F.gelu(self.linear6(vec_output))
+        # vec_output = F.gelu(self.linear7(vec_output))
+        return vec_output
+    
+    
---
+++ OLD_Simple_CNN_Results/L2-10e/main.py
@@ -0,0 +1,160 @@
+import sys
+import torchvision
+import torch
+import torchvision.transforms as tr
+from cnn import SimpleCNN
+from cnn_dropout import SimpleCNN_DO
+from transformer import VisualTransformer
+import tqdm
+
+
+def run_model(device, model, batch_size=144, learning_rate=3e-3, num_epochs=10, \
+              save_name='cnn_model_control.pth'):
+    """
+    Preprocesses CIFAR10 data, trains the given model, tests it, and saves
+    the final model as a .pth file with the given save_name.
+    Computes and returns useful metrics (training loss, testing loss, and testing accuracy)
+    :param device: Device on which to train the model, often
+     torch.device('cuda:insert_num') or torch.device('cpu')
+    :param model: A PyTorch model
+    :param batch_size: Number of images to compute gradients for before updating weights
+     Default of 144
+    :param learning_rate: Hyperparamter that sets the rate at which gradient descent is
+     performed. Default of 3e-3
+    :param num_epochs: Number of epochs to train the model for
+    :param save_name: Name of the file that will have the model's trainable parameters
+     after training is complete
+    :return: (average training loss, average testing loss, average testing accuracy)
+    """
+    
+    model.to(device)
+
+    transform = tr.Compose([tr.ToTensor(), tr.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    training_dataset = torchvision.datasets.CIFAR10(root='./data', train=True,\
+         download=True, transform=transform)
+    training_dataloader = torch.utils.data.DataLoader(training_dataset, \
+        batch_size=batch_size, shuffle=True, num_workers=2)
+
+    testing_dataset = torchvision.datasets.CIFAR10(root='./data', train=False, \
+        download=True, transform=transform)
+    testing_dataloader = torch.utils.data.DataLoader(testing_dataset, \
+        batch_size=batch_size, shuffle=False, num_workers=2)
+
+    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    
+    optimizer = torch.optim.Adam(model.parameters(), learning_rate, weight_decay=1e-5)
+    loss_func = torch.nn.CrossEntropyLoss()
+    train_loss = train(model, training_dataloader, loss_func, optimizer, num_epochs, device)
+    test_loss, accuracy = test(model, testing_dataloader, loss_func, device)
+    
+    torch.save(model.state_dict(), save_name)
+    
+    return train_loss, test_loss, accuracy
+
+
+def train(model, dataloader, loss_func, optimizer, num_epochs, device):
+    """
+    Trains the given model on the training dataset.
+    :param model: A PyTorch model
+    :param dataloader: A dataloader that contains the training dataset
+     in the format (image, target).
+    :param loss_func: A callable loss function (e.g. torch.nn.CrossEntropyLoss())
+    :param optimizer: A callable optimizer used to update the model's trainable parameters
+    :param num_epochs: Number of epochs to train the model for
+    :param device: Device on which to train the model, often
+     torch.device('cuda:insert_num') or torch.device('cpu')
+    :return: A scalar (average loss over all epochs).
+    """
+    total_loss = 0
+
+    model.train()
+    for epoch in range(num_epochs):
+        epoch_loss = 0
+
+        with tqdm.tqdm(dataloader, unit="batch") as tepoch:
+            for X, Y in tepoch:
+                tepoch.set_description(f"Epoch {epoch}")
+                X, Y = X.to(device), Y.to(device)
+                output = model(X)
+                optimizer.zero_grad()
+                loss = loss_func(output, Y)
+                loss.backward()
+                optimizer.step()
+
+                epoch_loss += loss.item() * X.shape[0]
+
+            epoch_loss = epoch_loss / len(dataloader.dataset)
+            print("Epoch {}: {}".format(epoch, epoch_loss))
+            total_loss += epoch_loss
+    
+    return total_loss / (num_epochs)
+        
+
+
+def test(model, dataloader, loss_func, device):
+    """
+    Computes the average loss and average accuracy of the given model on the test set.
+    :param model: A PyTorch model
+    :param dataloader: A dataloader that contains the testing dataset
+     in the format (image, target).
+    :param loss_func: A callable loss function (e.g. torch.nn.CrossEntropyLoss())
+    :param device: Device on which to evaluate the model, often
+     torch.device('cuda:insert_num') or torch.device('cpu')
+    :return: A tuple. (average loss, average accuracy)
+    """
+    
+    epoch_loss_sum = 0
+    epoch_correct_sum = 0
+
+    model.eval()
+    with tqdm.tqdm(dataloader, unit="batch") as tepoch:
+        for X, Y in tepoch:
+            tepoch.set_description(f"Test progress")
+            X, Y = X.to(device), Y.to(device)
+            output = model(X)
+            loss = loss_func(output, Y)
+            epoch_loss_sum += loss.item() * X.shape[0]
+            epoch_correct_sum += correct_predict_num(output, Y)
+
+        avg_loss = epoch_loss_sum / len(dataloader.dataset)
+        avg_accuracy = epoch_correct_sum / len(dataloader.dataset)
+
+    return avg_loss, avg_accuracy
+
+def correct_predict_num(logit, target):
+    """
+    Returns the number of correct predictions.
+    :param logit: 2D torch tensor of shape [n, class_num], where
+        n is the number of samples, and class_num is the number of classes (10 for MNIST).
+        Represents the output of the model.
+    :param target: 1D torch tensor of shape [n],  where n is the number of samples.
+        Represents the ground truth categories of images.
+    :return: A python scalar. The number of correct predictions.
+    """
+    
+    predictions = torch.argmax(logit, dim = 1)
+    predictions = torch.where(predictions == target, 1, 0)
+    return torch.sum(predictions).item()
+
+
+def main():
+    
+    model_types = {"CNN" : SimpleCNN(), "CNN_DO" : SimpleCNN_DO(), "TRANSFORMER" : VisualTransformer()}
+    
+    if len(sys.argv) != 2 or sys.argv[1] not in model_types.keys():
+        print("USAGE: python main.py <Model Type>")
+        print("<Model Type>: [CNN/CNN_DO/TRANSFORMER]")
+        exit()
+    
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    print("Device: ", device)
+    
+    model = model_types[sys.argv[1]]
+    
+    train_loss, test_loss, test_acc = run_model(device, model)
+    print("Training loss: {}".format(train_loss))
+    print("Testing Loss: {}".format(test_loss))
+    print("Testing Accuracy: {}".format(test_acc))
+
+if __name__ == "__main__":
+    main()
